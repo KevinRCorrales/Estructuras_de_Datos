859e920968fbbc1f287f37e4f7ce99d2c5dfa377
KevinRCorrales <kevin.ramirezcorr@campusucc.edu.co>
2026-02-11 11:23:30 -0500

    Formato a Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-	int[] a = {32, 43, 6, 7, 12, 0, 5};
-	for (int i=0; i<a.length; i++) {
-	    System.out.println(a[i]);
+	public static void main(String[] args) {
+		int[] a = { 32, 43, 6, 7, 12, 0, 5 };
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
 	}
-    }
 }
